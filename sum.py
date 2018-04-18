1ab2e2f3cf968eebfedacde77cd1f03ffe480156
SWATHI S <36816@fisat>
2018-04-18 01:18:58 -0400

    Create a function to add 4 no.

--- sum.py
+++ sum.py
@@ -6,6 +6,12 @@ def add3(a,b,c):
 	p=a+b+c
 	return p
 
+def add4(a,b,c,d):
+	y=a+b+c+d
+	return y
+
+print "Sum of 4 no.: ",add4(2,3,4,5)
+
 print "Sum of 2 no.: ",add(7,3)
 
 print "sum of 3 no.: ",add3(2,3,4)	
